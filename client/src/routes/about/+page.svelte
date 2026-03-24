d7385ab0cef62916997d7332faa508964731595c
eaindome <ekow.a.indome@gmail.com>
2026-03-24 12:46:26 +0000

    update: updates from miah

--- client/src/routes/about/+page.svelte
+++ client/src/routes/about/+page.svelte
@@ -377,9 +377,6 @@
 			<div class="flex flex-col sm:flex-row gap-4 justify-center">
 				<Button variant="inverse" size="lg" href="/booking" elevation>
 					Book an Appointment
-					<svg class="ml-2 h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
-					</svg>
 				</Button>
 				<Button variant="inverse-outline" size="lg" href="/services">
 					Our Services
